96c8ba979cb825a971803c2244f8010f45bffe2a
TAmyG <tamy.vivas@gmail.com>
2017-11-29 19:38:58 -0600

    implementación de onRequest

--- index.js
+++ index.js
@@ -3,13 +3,31 @@
 const Hapi = require('hapi');
 const server = new Hapi.Server();
 const db = require('./database').db;
+const jwt = require('jsonwebtoken');
+const secret = '123456';
+const objToken = {};
 const plugins = [{
     register: require('inert')
 }];
 
-
 server.connection({
     port: 3000
+});
+
+server.ext('onRequest', (request, reply) => {
+    if(request.url.path == '/createToken'){
+        return reply.continue();
+    }else{        
+        if(!request.headers.token){
+            return reply({error: 'Se requiere token'})
+        }
+        let payload = jwt.verify(request.headers.token, secret, {}, (err, payload)=>{
+            if(err){
+                return reply({error: err});
+            }            
+            return reply.continue();
+        });
+    }
 });
 
 //server.route(rutas);
